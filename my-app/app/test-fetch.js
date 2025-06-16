8e82a2592d859c943b81150a3dfb8caa7f6d5f89
JUAN MARTINEZ <163062478+juanmartinezjorcin@users.noreply.github.com>
2025-06-16 19:38:10 -0300

    api

--- my-app/app/test-fetch.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
-const API_URL = process.env.REACT_APP_API_URL;
-
-fetch(`${API_URL}/users`)
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
--- my-app/services/api.js
+++ my-app/services/api.js
@@ -1,16 +1,7 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+import { URL } from '@env';
 
-const URL = process.env.REACT_APP_API_URL;
-
-async function get(endpoint) {
+export async function get(endpoint) {
   const res = await fetch(`${URL}${endpoint}`);
   if (!res.ok) throw new Error(`Error: ${res.status}`);
   return res.json();
 }
-
-module.exports = { get };
-
-/*
-USO: en test-api.js
-
